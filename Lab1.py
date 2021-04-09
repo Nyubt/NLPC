7942deddf1751d1a8669dd799848274c34941e8c
Elena Ciumeica <e.w.child@gmail.com>
2021-04-09 17:31:29 +0300

    Lab1.py update

--- Lab1.py
+++ Lab1.py
@@ -1,9 +1,9 @@
 luna=int(input('Introduceti luna'))
 an=int(input('Introduceti anul'))
 #afisat numarul de zile din luna
-if luna == 1 or luna == 3 or luna == 5 or luna == 7 or luna == 8 or luna == 10 or luna == 12:
+if luna in [1,3,5,7,8,10,12]:
     print('31 zile')
-elif luna == 4 or luna == 6 or luna == 9 or luna == 11:
+elif luna in [4,6,9,11]:
     print('30 zile')
 elif luna == 2:
     if an%4 == 0 and an%100 != 0:
